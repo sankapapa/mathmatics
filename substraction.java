95c1f327dbad21585bf0a54bda3dba7fac614a3c
sankapapa <pavankumar8485@gmail.com>
2023-09-15 19:20:34 +0530

    substraction

---
+++ substraction.java
@@ -0,0 +1,9 @@
+public class substraction {
+    public static void main(String[] args) {
+	    int x=50;
+	    int y=20;
+	    
+	        System.out.println("sum of x-y; " +(x-y));
+	     }
+    
+}
