c395102ac9af9cf9f9b05ca9901036f6633a2cef
sankapapa <pavankumar8485@gmail.com>
2023-09-15 19:27:32 +0530

    multiplication

---
+++ multiplication.java
@@ -0,0 +1,9 @@
+public class multiplication {
+    
+    public static void main(String[] args) {
+	    int c=10;
+	    int d=150;
+	    
+	        System.out.println("multiplication of c*d; " +(c*d));
+	     }
+}
