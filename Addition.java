6f4a45fa0e8c49fd16cac9326752913e8b67c07a
sankapapa <pavankumar8485@gmail.com>
2023-09-15 18:52:35 +0530

    addition

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class addition {
+   
+	public static void main(String[] args) {
+	    int a=500;
+	    int b=100;
+	    
+	        System.out.println("sum of a+b; " +(a+b));
+	     }
+	}
+
+    
+
